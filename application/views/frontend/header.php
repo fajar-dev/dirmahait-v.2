6c530365db0421a71718ebb848a399d62f1d45d4
faja rivaldi chan <fajarrivaldi2015@gmail.com>
2022-09-19 03:17:36 +0700

    loading load

--- application/views/frontend/header.php
+++ application/views/frontend/header.php
@@ -53,6 +53,7 @@
         <span></span>
         <span></span>
       </div>
+      <h1 class="text-primary fw-semibold mt-4 pt-4">Loading...</h1>
     </div>
   </div>
   <!-- ***** Preloader End ***** -->
